0f1a280f0b55f9c0b2c6bcca60c18b65fddd4b1c
IconicDexter <kennymwendwa67@gmail.com>
2023-06-10 21:03:27 +0300

    Toolbar cleanup

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -15,28 +15,84 @@ import { LuHeading1, LuHeading2, LuHeading3 } from "react-icons/lu";
 
 type HeadingTagType = "h1" | "h2" | "h3";
 
+interface IHeading {
+  id: number;
+  tag: HeadingTagType;
+  icon: JSX.Element;
+  text: string;
+}
+
+const HeadingTags: IHeading[] = [
+  { id: 1, tag: "h1", icon: <LuHeading1 />, text: "Heading 1" },
+  { id: 2, tag: "h2", icon: <LuHeading2 />, text: "Heading 2" },
+  { id: 3, tag: "h3", icon: <LuHeading3 />, text: "Heading 3" },
+];
+
 function HeadingToolbarPlugin(): JSX.Element {
   const [editor] = useLexicalComposerContext();
-  const HeadingTags = ["h1", "h2", "h3"];
-  const onClick = (tag: HeadingTagType): void => {
+  const [selectedOption, setSelectedOption] = useState<IHeading>(
+    HeadingTags[0]
+  );
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const handleOptionClick = (heading: IHeading) => {
+    setSelectedOption(heading);
+    setIsOpen(false);
     editor.update(() => {
       const selection = $getSelection();
       if ($isRangeSelection(selection)) {
-        $setBlocksType(selection, () => $createHeadingNode(tag));
+        $setBlocksType(selection, () => $createHeadingNode(heading.tag));
       }
     });
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
     <>
-      {HeadingTags.map((tag) => (
+      <div className="relative" ref={dropdownRef}>
         <button
-          className="rounded-lg bg-blue-600 px-3 py-2 text-white"
-          onClick={() => onClick(tag as HeadingTagType)}
-          key={tag}>
-          {tag.toUpperCase()}
+          id="dropdownHoverButton"
+          className="text-gray-900 bg-gray-50 hover:bg-gray-200 focus:ring-4 focus:outline-none border border-gray-300 focus:ring-gray-300 font-medium rounded-lg text-lg px-3 py-2.5 text-center inline-flex items-center"
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}>
+          {selectedOption.icon}
+          <RxCaretDown className="w-6 h-6 ml-1" />
         </button>
-      ))}
+        {isOpen && (
+          <div className="absolute top-2 left-0 z-10 mt-10 bg-gray-50 divide-y divide-gray-100 rounded-lg shadow-sm">
+            <ul className="text-sm text-gray-700">
+              {HeadingTags.map((heading) => (
+                <li key={heading.id}>
+                  <button
+                    type="button"
+                    onClick={() => handleOptionClick(heading)}
+                    className={`${
+                      selectedOption.id === heading.id ? "bg-gray-200" : ""
+                    } inline-flex w-36 pl-2 items-center py-2 text-sm text-gray-700 hover:bg-gray-200`}>
+                    <div className="mr-2 text-lg">{heading.icon}</div>{" "}
+                    {heading.text}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </>
   );
 }
@@ -69,115 +125,11 @@ function ListToolbarPlugin(): JSX.Element {
   );
 }
 
-function HeadingsToolbarPlugin(): JSX.Element {
-  const [editor] = useLexicalComposerContext();
-  const HeadingTags = ["h1", "h2", "h3"];
-
-  const onChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-    const tag = event.target.value as HeadingTagType;
-    editor.update(() => {
-      const selection = $getSelection();
-      if ($isRangeSelection(selection)) {
-        $setBlocksType(selection, () => $createHeadingNode(tag));
-      }
-    });
-  };
-
-  return (
-    <select
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      onChange={onChange}>
-      {HeadingTags.map((tag) => (
-        <option value={tag} key={tag}>
-          {tag.toUpperCase()}
-        </option>
-      ))}
-    </select>
-  );
-}
-
-interface IHeading {
-  id: number;
-  tag: JSX.Element;
-  text: string;
-}
-
-const HeadingTags: IHeading[] = [
-  { id: 1, tag: <LuHeading1 />, text: "Heading 1" },
-  { id: 2, tag: <LuHeading2 />, text: "Heading 2" },
-  { id: 3, tag: <LuHeading3 />, text: "Heading 3" },
-];
-
-function HeadingProto(): JSX.Element {
-  const [editor] = useLexicalComposerContext();
-  const [selectedOption, setSelectedOption] = useState<IHeading>(
-    HeadingTags[0]
-  );
-  const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
-  const handleOptionClick = (heading: IHeading) => {
-    setSelectedOption(heading);
-    setIsOpen(false);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      dropdownRef.current &&
-      !dropdownRef.current.contains(event.target as Node)
-    ) {
-      setIsOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-  return (
-    <>
-      <div className="relative" ref={dropdownRef}>
-        <button
-          id="dropdownHoverButton"
-          className="text-gray-900 bg-gray-50 hover:bg-gray-200 focus:ring-4 focus:outline-none border border-gray-300 focus:ring-gray-300 font-medium rounded-lg text-lg px-3 py-2.5 text-center inline-flex items-center"
-          type="button"
-          onClick={() => setIsOpen(!isOpen)}>
-          {selectedOption.tag}
-          <RxCaretDown className="w-6 h-6 ml-1" />
-        </button>
-        {isOpen && (
-          <div className="absolute top-0 left-0 z-10 mt-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm">
-            <ul className="text-sm text-gray-700">
-              {HeadingTags.map((heading) => (
-                <li key={heading.id}>
-                  <button
-                    type="button"
-                    onClick={() => handleOptionClick(heading)}
-                    className={`${
-                      selectedOption.id === heading.id ? "bg-gray-100" : ""
-                    } inline-flex w-36 pl-2 items-center py-2 text-sm text-gray-700 hover:bg-gray-100`}>
-                    <div className="mr-2 text-lg">{heading.tag}</div>{" "}
-                    {heading.text}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    </>
-  );
-}
-
 export default function ToolbarPlugin(): JSX.Element {
   return (
     <div className="flex items-center space-x-2">
       <HeadingToolbarPlugin />
-      <HeadingsToolbarPlugin />
       <ListToolbarPlugin />
-      <HeadingProto />
     </div>
   );
 }
