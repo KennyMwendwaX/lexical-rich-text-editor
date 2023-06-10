b83697b055cc1d3a28a4786128d099f1cf416f0a
IconicDexter <kennymwendwa67@gmail.com>
2023-06-10 16:38:48 +0300

    Toolbar cleanup

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -144,8 +144,6 @@ function HeadingProto(): JSX.Element {
           className="text-gray-900 bg-gray-50 hover:bg-gray-200 focus:ring-4 focus:outline-none border border-gray-300 focus:ring-gray-300 font-medium rounded-lg text-lg px-3 py-2.5 text-center inline-flex items-center"
           type="button"
           onClick={() => setIsOpen(!isOpen)}>
-          {/* <div className="mr-2"> {selectedOption.tag}</div>
-          {selectedOption.text} */}{" "}
           {selectedOption.tag}
           <RxCaretDown className="w-6 h-6 ml-1" />
         </button>
